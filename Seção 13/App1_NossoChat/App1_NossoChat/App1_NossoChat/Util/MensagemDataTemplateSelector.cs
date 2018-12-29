c4ac8ae77c06668f4f0823aaf0c2f82809bc295d
marcellm <marcell.marchetto@gmail.com>
2018-12-28 22:57:34 -0200

    OK

---
+++ Seção 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/Util/MensagemDataTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using App1_NossoChat.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App1_NossoChat.Util
+{
+    public class MensagemDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate MinhasMensagensTemplate { get; set; }
+        public DataTemplate MensagensOutrasPessoasTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            try
+            {
+                var Usuario = UsuarioUtil.GetUsuarioLogado();
+
+                return ((Mensagem)item).id_usuario == Usuario.id ? MinhasMensagensTemplate : MensagensOutrasPessoasTemplate;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
